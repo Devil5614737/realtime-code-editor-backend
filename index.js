f3b2e87e407a8eba9e3b08aaaeb5a7c16d6b13dc
Devil5614737 <kaushiksheel9@gmail.com>
2023-01-06 21:10:32 +0530

    '.'

---
+++ index.js
@@ -0,0 +1,82 @@
+
+const express=require('express')
+const app=express();
+const cors=require('cors');
+
+
+
+app.use(cors())
+
+
+app.use(express.json())
+
+
+
+
+
+const PORT = 4000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://127.0.0.1:5173",
+    // credentials: true,
+  },
+});
+
+
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+    // Map
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+        (socketId) => {
+            return {
+                socketId,
+                name: userSocketMap[socketId],
+            };
+        }
+    );
+}
+
+
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+
+
+
+  socket.on("join room", (room) => {
+
+     userSocketMap[socket.id] = room.name;
+        socket.join(room?.uuid);
+        const clients = getAllConnectedClients(room?.uuid)
+clients.forEach(({ socketId }) => {
+            io.to(socketId).emit('joined', {
+                clients,
+                name:socket?.name,
+                socketId: socket?.id,
+            });
+        });
+    
+  });
+
+
+
+socket.on("new input", (data) => {
+
+  socket.in(data?.uuid).emit('input recieved',data?.code)
+
+  });
+
+  socket.off("setup", (userData) => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData?.uuid);
+  });
+});
+
